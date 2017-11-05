6b9317f5ac7407a36c1deb88ede51d93e3e53233
michalmanski <michal.razor@gmail.com>
2017-11-05 21:12:23 +0100

    Dodaj pliki projektów.

---
+++ WinBtControler/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace WinBtControler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tył = new System.Windows.Forms.Button();
+            this.prawo = new System.Windows.Forms.Button();
+            this.lewo = new System.Windows.Forms.Button();
+            this.przod = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.tył);
+            this.groupBox2.Controls.Add(this.prawo);
+            this.groupBox2.Controls.Add(this.lewo);
+            this.groupBox2.Controls.Add(this.przod);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // tył
+            // 
+            resources.ApplyResources(this.tył, "tył");
+            this.tył.Name = "tył";
+            this.tył.UseVisualStyleBackColor = true;
+            this.tył.Click += new System.EventHandler(this.tył_Click);
+            this.tył.MouseDown += new System.Windows.Forms.MouseEventHandler(this.tył_MouseDown);
+            this.tył.MouseUp += new System.Windows.Forms.MouseEventHandler(this.tył_MouseUp);
+            // 
+            // prawo
+            // 
+            resources.ApplyResources(this.prawo, "prawo");
+            this.prawo.Name = "prawo";
+            this.prawo.UseVisualStyleBackColor = true;
+            this.prawo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.prawo_MouseDown);
+            this.prawo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.prawo_MouseUp);
+            // 
+            // lewo
+            // 
+            resources.ApplyResources(this.lewo, "lewo");
+            this.lewo.Name = "lewo";
+            this.lewo.UseVisualStyleBackColor = true;
+            this.lewo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lewo_MouseDown);
+            this.lewo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lewo_MouseUp);
+            // 
+            // przod
+            // 
+            resources.ApplyResources(this.przod, "przod");
+            this.przod.Name = "przod";
+            this.przod.UseVisualStyleBackColor = true;
+            this.przod.MouseDown += new System.Windows.Forms.MouseEventHandler(this.przod_MouseDown);
+            this.przod.MouseUp += new System.Windows.Forms.MouseEventHandler(this.przod_MouseUp);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Maximum = 250;
+            this.progressBar1.Name = "progressBar1";
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.groupBox4);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Controls.Add(this.progressBar1);
+            this.groupBox4.ForeColor = System.Drawing.Color.DarkRed;
+            resources.ApplyResources(this.groupBox4, "groupBox4");
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.TabStop = false;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.HotkeyField;
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button tył;
+        private System.Windows.Forms.Button prawo;
+        private System.Windows.Forms.Button lewo;
+        private System.Windows.Forms.Button przod;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WinBtControler/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace WinBtControler
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+
+
+            InitializeComponent();
+            wolnePorty();
+            button3.Enabled = false;
+            button4.Enabled = false;
+
+        } 
+        void wolnePorty()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text == "")
+                {
+                    MessageBox.Show("Musisz wybrać port!");
+                }
+                else
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = 4800;
+                    serialPort1.Open();
+                    serialPort1.Write("x");
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    comboBox1.Enabled = false;
+                    label2.Text = "POŁĄCZONY";
+                    label2.ForeColor = System.Drawing.Color.Green;
+                    
+                    groupBox2.Enabled = true;
+                    textBox1.Enabled = true;
+                    button2.Visible = true;
+                    button1.Visible = false;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+
+                    //wylaczyc przyciski
+
+
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+
+            }
+        }
+
+        private void tył_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            label2.Text = "ROZŁĄCZONY";
+            label2.ForeColor = System.Drawing.Color.Red;
+            groupBox2.Enabled = false;
+            comboBox1.Enabled = false;
+            button2.Enabled = false;
+            button1.Visible = true;
+            button2.Visible = false;
+            button1.Enabled = true;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.ForeColor = System.Drawing.Color.Red;
+            progressBar1.Value = 200;
+            groupBox4.Text = "Prędkość: " + ((progressBar1.Value / 250.0) * 100) + "%";
+            comboBox1.Text = "COM3";
+        }
+
+        private void prawo_MouseDown(object sender, MouseEventArgs e)
+        {
+            serialPort1.Write("R"); //PRAWO - RIGHT
+        }
+
+        private void prawo_MouseUp(object sender, MouseEventArgs e)
+        {
+            serialPort1.Write("X");
+            //int MyInt =10;
+
+            //byte[] b = BitConverter.GetBytes(MyInt);
+            //serialPort1.Write(b, 0, 4);
+        }
+
+
+
+        private void lewo_MouseDown(object sender, MouseEventArgs e)
+        {
+            serialPort1.Write("L"); //LEWO - LEFT
+        }
+
+        private void lewo_MouseUp(object sender, MouseEventArgs e)
+        {
+            serialPort1.Write("X");
+        }
+
+        private void przod_MouseDown(object sender, MouseEventArgs e)
+        {
+            serialPort1.Write("F"); //FRONT - PRZOD
+        }
+
+        private void przod_MouseUp(object sender, MouseEventArgs e)
+        {
+            serialPort1.Write("X");
+        }
+
+        private void tył_MouseDown(object sender, MouseEventArgs e)
+        {
+            serialPort1.Write("B"); //BACK - TYŁ
+        }
+
+        private void tył_MouseUp(object sender, MouseEventArgs e)
+        {
+            serialPort1.Write("X");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value >= 250)
+                progressBar1.Value = 250;
+            else
+                progressBar1.Value += 10;
+            groupBox4.Text = "Prędkość: " + (progressBar1.Value / 250.0) * 100 + "%";
+            serialPort1.Write("S");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value <= 0)
+                progressBar1.Value = 0;
+            else
+                progressBar1.Value -= 10;
+            groupBox4.Text = "Prędkość: " + (progressBar1.Value / 250.0) * 100 + "%";
+            serialPort1.Write("s");
+
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            string x = Convert.ToString(MousePosition.X);
+            string y = Convert.ToString(MousePosition.Y);
+            label3.Text = ("POZ X: " + x + " POZ Y: " + y);
+
+
+        }
+
+       
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                serialPort1.Write("F"); //FRONT - PRZOD
+                
+                
+
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                serialPort1.Write("B"); //BACK - TYL
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                serialPort1.Write("L"); // LEFT
+
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                serialPort1.Write("R");
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            serialPort1.Write("X");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}   
+
